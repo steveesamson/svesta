204ecf459c2bdfc96f5a3cc375cd11b49f6c65b7
Steve Samson <stevee.samson@gmail.com>
2024-08-05 12:54:37 -0400

    fix TransportFactory export error

--- docs/_app/immutable/entry/start.B_zKs1Ch.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.Dh74fBqn.js";export{t as start};
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -9,7 +9,7 @@ import Resource from "./components/resource.svelte";
 import Offline from "./components/offline.svelte";
 import BlockLoader from "./components/block-loader.svelte";
 import Loader from "./components/loader.svelte";
-import type { TransportType } from "./types/index.js";
-const Transport = (BulkTransport as unknown) as TransportType;
+import type { TransportFactory } from "./types/index.js";
+const Transport = (BulkTransport as unknown) as TransportFactory;
 
 export { Resource, Offline, BlockLoader, Loader, Transport }
